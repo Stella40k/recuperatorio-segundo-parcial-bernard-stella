2378a9f62e4b775f2a87ee4d5c88061e46ea9c81
Bernard <bernardstella0704@gmail.com>
2025-07-10 14:47:39 -0300

    feat: estructura de cnfig

---
+++ src/config/database.js
@@ -0,0 +1,21 @@
+import { Sequelize } from "sequelize";
+import "dotenv/config";
+
+export const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.USER,
+    process.env.DB_PASSWORD,
+    {
+        host:process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+    }
+);
+
+export const connect = async() => {
+    try {
+        await sequelize.sync({force: true})
+        console.log("conectado a la base de datoss");
+    } catch (error) {
+        console.log("error al conectar con la bd", error);
+    }
+}
