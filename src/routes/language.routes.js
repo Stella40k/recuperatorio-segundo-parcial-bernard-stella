57948df02140437673dfcd8303863a3c681706c6
Bernard <bernardstella0704@gmail.com>
2025-07-10 15:09:44 -0300

    feat: estructura de las rutas

---
+++ src/routes/language.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+//falta la importacion de controladores
+export const routes = Router()
+
+//faltan las funciones o rutas de los controles
+routes.get("/languages", )
+routes.get("/languages/:id", )
+routes.post("/languages", )
+routes.put("/languages/:id", )
+routes.delete("/languages/:id", )
