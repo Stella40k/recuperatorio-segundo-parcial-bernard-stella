a35302ab06f406b4f4739f5205effcf519e45aad
Bernard <bernardstella0704@gmail.com>
2025-07-10 15:01:43 -0300

    fix: process.env.USER = process.env.DB_USSER. se logro la conexion a la bd

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import "dotenv/config";
-import { connect } from "./src/config/database";
+import { connect } from "./src/config/database.js";
 
 const app = express();
 const PORT = process.env.PORT;
--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 export const sequelize = new Sequelize(
     process.env.DB_NAME,
-    process.env.USER,
+    process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
         host:process.env.DB_HOST,
